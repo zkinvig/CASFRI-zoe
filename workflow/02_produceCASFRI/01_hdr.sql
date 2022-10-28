de7436475b984eb797c74d1561c13bf984bbad13
mtcarto <morgan.thomps@gmail.com>
2022-10-28 14:12:21 -0700

    add NS04, PE02, PE03 entries to hdr table

--- workflow/02_produceCASFRI/01_hdr.sql
+++ workflow/02_produceCASFRI/01_hdr.sql
@@ -46,8 +46,8 @@ FROM translation.inventory_list_cas05
 WHERE inventory_id IN ('AB03', 'AB06', 'AB07', 'AB08', 'AB10', 'AB11', 'AB16', 'AB25', 'AB27','AB29', 'AB30', 
                        'AB31', 'AB32', 'AB33', 'BC08', 'BC10', 'BC11', 'BC12','BC13', 'BC14'
                        'MB01', 'MB02', 'MB04', 'MB05', 'MB06', 'MB07', 
-                       'NB01', 'NB02', 'NL01', 'NS01', 'NS02', 'NS03', 'NT01', 'NT03', 'NT04',
-                       'ON01', 'ON02', 'PC01', 'PC02', 'PE01', 
+                       'NB01', 'NB02', 'NL01', 'NS01', 'NS02', 'NS03','NS04', 'NT01', 'NT03', 'NT04',
+                       'ON01', 'ON02', 'PC01', 'PC02', 'PE01', 'PE02', 'PE03'
                        'QC01', 'QC02', 'QC03', 'QC04', 'QC05', 'QC06', 'QC07', 
                        'SK01', 'SK02', 'SK03', 'SK04', 'SK05', 'SK06', 
                        'YT01', 'YT02', 'YT03');
