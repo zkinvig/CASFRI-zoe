ce6a8a1b20ff78f8627fdcc2a62a94752a17b324
Zoe Kinvig <zoe.kinvig@nrcan-rncan.gc.ca>
2024-10-24 12:20:23 -0700

    Trying a nb06 ingestion fix

--- conversion/sh/load_nb06.sh
+++ conversion/sh/load_nb06.sh
@@ -31,7 +31,9 @@ fullTargetTableName=$targetFRISchema.$inventoryID
 -nln $fullTargetTableName $layer_creation_options $other_options \
 -nlt PROMOTE_TO_MULTI -nlt CONVERT_TO_LINEAR \
 -emptyStrAsNull \
--sql "SELECT *, '$srcFilename' AS src_filename, '$inventoryID' AS inventory_id FROM $srcLayerName WHERE holder IS NULL OR holder NOT IN (16, 20)" \
+-sql "SELECT *, '$srcFilename' AS src_filename, '$inventoryID' AS inventory_id FROM $srcLayerName WHERE holder IS NULL OR holder NOT IN (16, 20);
+ALTER TABLE $fullTargetTableName ALTER COLUMN stdlab TYPE TEXT;
+UPDATE $fullTargetTableName SET stdlab = '' WHERE stdlab IS NULL;" \
 -progress $overwrite_tab
 
 source ./common_postprocessing.sh
